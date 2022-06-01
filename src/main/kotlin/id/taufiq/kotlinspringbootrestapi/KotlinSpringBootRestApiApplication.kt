505cc865947504764e78cbbf23459028dc9a316b
taufiq <taufiq.goe@gmail.com>
2022-06-01 13:44:21 +0800

    Initial commit

---
+++ src/main/kotlin/id/taufiq/kotlinspringbootrestapi/KotlinSpringBootRestApiApplication.kt
@@ -0,0 +1,11 @@
+package id.taufiq.kotlinspringbootrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBootRestApiApplication>(*args)
+}
---
+++ src/test/kotlin/id/taufiq/kotlinspringbootrestapi/KotlinSpringBootRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package id.taufiq.kotlinspringbootrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
